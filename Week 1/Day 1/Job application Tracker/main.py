e3e3bc4b339e7ad07db2547e82c10cf2dd16f602
Jamie Leeuw <mrjtleeuw27@gmail.com>
2026-09-16 21:17:53 +0200

    Initial commit

---
+++ Week 1/Day 1/Job application Tracker/main.py
@@ -0,0 +1,66 @@
+import json
+
+#Add Job applications
+def add():
+  data = {}
+  data['ID'] = len(data) + 1
+  data['Company'] = input('Company: ')
+  data['Position'] = input('Position: ')
+  data['Location'] = input('Location: ')
+  data['Status'] = input('Status: ')
+  
+  with open('data_file.json',"w") as file:
+      json.dump(data,file)
+
+
+
+#View one Job application
+def view():
+    with open('data_file.json', 'r') as file:
+        data = json.load(file)
+    print(data)
+
+
+#Search for Job applications
+
+
+
+#Update Job application
+
+
+#Delete a job application
+
+
+#View all the Job applications
+
+
+
+
+
+
+#Create Menu to display Option to Choose
+def menu():
+    
+    while True:
+        user_input = int(input("Choose a Option 1-7: "))
+        if user_input == 1:
+            add()
+            print('Your application has been added')
+        elif user_input == 2:
+            view()
+            print("View") 
+        elif user_input == 3:
+            print("Search")
+        elif user_input == 4:
+            print("Update") 
+        elif user_input == 5:
+            print("Delete") 
+        elif user_input == 6:
+            print("Show")
+        elif user_input == 7:
+            print("Thank you for using my CLI program")
+            break
+        else:
+            print("You can only enter a number between 1 - 7")
+
+menu()
