9f230b76fdc1d1a17f5b45715c217d1a473f3210
Jamie Leeuw <mrjtleeuw27@gmail.com>
2026-09-23 23:27:46 +0200

    Database Setup

--- Week 1/Day 2 & 3/database/db.py
+++ Week 1/Day 2 & 3/database/db.py
@@ -6,12 +6,12 @@ def create_connection():
 
     #Create cursor to be able to do functionality to database
     cur = conn.cursor()
+    #To be to create relationships between tables
+    cur.execute('PRAGMA foreign_keys = ON;')
     return conn, cur
 
 def create_tables():
     conn,cur = create_connection()
-    #To be to create relationships between tables
-    cur.execute('PRAGMA foreign_keys = ON;')
 
     #Create users Database table
     cur.execute("""CREATE TABLE IF NOT EXISTS users (
@@ -40,5 +40,58 @@ def create_user(name,surname):
     conn.close()
     return cur.lastrowid
 
+
+def create_todo(title,user_id):
+    conn,curr = create_connection()
+    curr.execute('INSERT INTO todos (title,user_id) VALUES(?,?)', (title,user_id))
+
+    conn.commit()
+    conn.close()
+
+    return curr.lastrowid
+
+def get_user(userid):
+    conn,curr = create_connection()
+    curr.execute('SELECT * FROM users WHERE id = ?',(userid,))
+
+    users = curr.fetchone()
+    conn.close()
+    return users
+
+def get_todos():
+    conn,curr = create_connection()
+    curr.execute('SELECT * FROM todos')
+
+    todo = curr.fetchall()
+    conn.close()
+    return todo
+
+def get_todo_by_id(todo_id):
+    conn,curr = create_connection()
+    curr.execute('SELECT * FROM todos WHERE todo_id = ?', (todo_id,))
+
+    todo = curr.fetchone()
+    conn.close()
+    return todo
+
+def update_todo(todo_id,title,complete_status):
+    conn,curr = create_connection()
+
+    sql_prompt = """UPDATE todos
+                    SET (title,complete_status) = (?,?)
+                    WHERE todo_id = ?
+    """
+    curr.execute(sql_prompt,(title,complete_status,todo_id))
+    conn.commit()
+    conn.close()
+    return True
+
+def delete_todo(todo_id):
+    conn,curr = create_connection()
+
+    curr.execute('DELETE FROM todos WHERE todo_id = ?',(todo_id,))
+    conn.commit()
+    conn.close()
+    return True
 
 create_tables()
